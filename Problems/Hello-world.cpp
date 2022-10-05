5ea4d4d174425718c111588453c5f3af79174a16
Ashiiidev <87383186+Ashiiidev@users.noreply.github.com>
2022-10-05 07:54:24 +0530

    Create Hello-world.cpp
    This is a program to create hello world in c++
    Please accept my pr

---
+++ Problems/Hello-world.cpp
@@ -0,0 +1,8 @@
+# this is a program for crrating hello world in c++
+
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "Hello, World!" << endl; // This prints Hello, World!
+   return 0;
+}
