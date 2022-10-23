fd262ad1147c6796d46fa9dffc05fd6339335042
MUHAMMAD AQEEL <113312043+Aqeel-Mubarak@users.noreply.github.com>
2022-10-23 11:52:03 -0700

    Update Pattern with recursion in python.py

--- Pattern with recursion in python.py
+++ Pattern with recursion in python.py
@@ -1,13 +1,11 @@
 def main():
     x = int(input("Enter no of lines: "))
     staeric(x)
-
 def staeric(n, j=1):
     if j == n:
         print("*"*n)
         return
     print("*"*j)
     return staeric(n, j+1)
-
 if __name__ == "__main__":
     main()
