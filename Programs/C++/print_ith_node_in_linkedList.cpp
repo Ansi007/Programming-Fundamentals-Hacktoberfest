b2e62252e3ae68a3d83920667527686e3b62b50a
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-10-29 23:48:14 +0530

    Create print_ith_node_in_linkedList.cpp

---
+++ Programs/C++/print_ith_node_in_linkedList.cpp
@@ -0,0 +1,118 @@
+/* 
+Submitted by: Sneha Jaiswal
+GitHub Link: https://github.com/Sneha-jais
+Problem Statement:
+For a given a singly linked list of integers and a position 'i', print the node data at the 'i-th' position.
+Note :
+Assume that the Indexing for the singly linked list always starts from 0.
+
+If the given position 'i',  is greater than the length of the given singly linked list, then don't print anything.
+
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first line of each test case or query contains the elements of the singly linked list separated by a single space.
+
+The second line contains the value of 'i'. It denotes the position in the given singly linked list.
+
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
+
+Output format :
+For each test case, print the node data at the 'i-th' position of the linked list(if exists).
+
+Output for every test case will be printed in a seperate line.
+
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^5
+i  >= 0
+Time Limit: 1sec
+
+Sample Input 1 :
+1
+3 4 5 2 6 1 9 -1
+3
+
+Sample Output 1 :
+2
+
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+
+};
+
+
+Node* takeInput(){
+    int data;
+cin>>data;
+Node *head=NULL;
+Node *tail=NULL;
+//-1 is terminating condition
+
+while(data!=-1){
+    Node *new_node=new Node(data);
+    if(head==NULL){
+head=new_node;
+tail=new_node;
+    }
+    else{
+        tail->next=new_node;
+        tail=tail->next;
+    }
+    cin>>data;
+   
+}
+return head;
+}
+
+// void print(Node *head){
+//     while(head!=NULL){
+//         cout<<head->data<<" ";
+//         head=head->next;
+//     }
+//     cout<<endl;
+
+// }
+
+void printi(Node *head,int i){
+   Node *temp=head;
+    int count=0;
+    while(count<i){
+        temp=temp->next;
+      count++;
+    }
+
+    cout<<temp->data<<endl;
+}
+
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+   
+   Node *head=takeInput();
+//    print(head);
+     int i;
+    cin>>i;
+   
+   printi(head,i);
+  
+
+}
+
+    return 0;
+}
