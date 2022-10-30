14862f7e71c8fe58824cdef4b9812038e821ebca
Anurag Kainth <94080341+anuragkainth@users.noreply.github.com>
2022-10-30 17:32:32 +0530

    Optimized Approach Factorial using Recursion
    Optimized Approach Factorial using Recursion

--- Programs/C++/Factorial.cpp
+++ Programs/C++/Factorial.cpp
@@ -1,3 +1,23 @@
+// Optimized Approach using Recursion
+#include<iostream>
+using namespace std;
+
+int fact_optimized(int x){
+	
+	if(x==0) return 1;
+	if(x==1) return 1;
+	return x * fact_optimized(x-1);
+}
+int main()
+{
+	int x;
+	cout << "Enter number :";
+	cin >> x;
+	cout<<"Factorial of "<<x<<" :"<<fact_optimized(x)<<endl;
+	return 0;	
+}
+
+// Direct Approach
 #include<iostream>
 using namespace std;
 int main()
